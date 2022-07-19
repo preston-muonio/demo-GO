1e6e2e083731d137f1cee9ba10d602488df586fc
preston-muonio <preston.muonio@gmail.com>
2022-07-19 18:14:14 -0500

    Updated main.go
    added features able to display multiple points

--- main.go
+++ main.go
@@ -4,20 +4,36 @@ import (
 	"fmt"
 )
 
-func main() {
-	var x int
-	fmt.Scan(&x)
-	fmt.Println("read number", x, "from stdin")
-	var y int
-	fmt.Scan(&y)
-	fmt.Println("read number", y, "from stdin")
+var y int
+var x int
+var o [3]int
+var p [3]int
 
+func main() {
+	var c int
+	fmt.Println("Number of cells")
+	fmt.Scan(&c)
+	for i := 0; i < (c); i++ {
+		fmt.Println("Cell:", i)
+
+		//ask for x value
+		fmt.Println("X of cell", i)
+		fmt.Scan(&x)
+		o[i] = x
+		//ask for the y value
+		fmt.Println("Y of cell", i)
+		fmt.Scan(&y)
+		p[i] = y
+	}
+	//prints grid
 	for i := 0; i < 3; i++ {
 		var a [3]int
-		if i == y {
-			a[x] = 1
+		l := i
+		for i := 0; i < (c); i++ {
+			if l == (p[i]) {
+				a[(o[i])] = 1
+			}
 		}
 		fmt.Println(a)
 	}
-
 }
